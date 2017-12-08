d981375f9fac7483cb4e0b8beded5545d54bec08
Ryder T Swan <oars@bartswan.com>
2017-12-07 21:07:04 -0600

    All RollingAverage class
    Efficiently computes and returns the rolling average of a double

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/RollingAverage.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.Utilities;
+
+import java.util.Vector;
+
+/**
+ * Created by ryderswan on 12/7/17.
+ */
+
+public class RollingAverage {
+    private Vector<Double> signal = new Vector<Double>();
+    private double sum = 0.0;
+
+    public int samples = 10;
+
+    Double update(Double current_value)
+    {
+        signal.add(current_value);
+        sum += current_value;
+
+        if (signal.size() > samples) {
+            // remove oldest value
+            sum -= signal.get(0);
+            signal.remove(0);
+        }
+
+        return sum / signal.size();
+    }
+}
