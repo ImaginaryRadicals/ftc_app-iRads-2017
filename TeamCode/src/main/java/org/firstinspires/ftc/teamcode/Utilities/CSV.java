3f3442cd9bbc5d49ed96549f1afb1c0cbd76912d
Stephen Berry <stephenberry.developer@gmail.com>
2017-12-12 20:41:28 -0600

    Added WIP CSV writer

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/CSV.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.Utilities;
+
+import org.firstinspires.ftc.teamcode.RobotHardware;
+
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Vector;
+
+/**
+ * Created by HomeStephen on 12/12/17.
+ */
+
+public class CSV {
+    private RobotHardware robotHardware;
+    private Vector<Vector<Double>> data = new Vector<>();
+    private Vector<String> titles = new Vector<>();
+
+    CSV(RobotHardware robotHardware)
+    {
+        this.robotHardware = robotHardware;
+    }
+
+    void add(Vector<Double> new_data)
+    {
+        data.add(new_data);
+    }
+    void add(double[] new_data)
+    {
+        Vector<Double> vec = new Vector<>();
+        for (double value : new_data)
+            vec.add(value);
+        data.add(vec);
+    }
+
+    void csv(String fileName)
+    {
+        csv(fileName, new Vector<String>());
+    }
+
+    void csv(String fileName, Vector<String> titles)
+    {
+        try{
+            FileOutputStream outputStream = robotHardware.hardwareMap.appContext.openFileOutput(fileName, Context.MODE_PRIVATE);
+
+            PrintWriter writer = new PrintWriter(outputStream);
+
+            for (int i = 0; i < titles.size(); ++i)
+            {
+                writer.print(titles.get(i));
+                if (i < titles.size() - 1)
+                    writer.print(", ");
+            }
+            if (titles.size() > 0)
+                writer.println();
+
+            int n_steps = data.size();
+            for (int i = 0; i < n_steps; ++i)
+            {
+                int n_states = data.firstElement().size();
+                for (int j = 0; j < n_states; ++j)
+                {
+                    writer.print(data.get(i).get(j));
+                    if (j < n_states - 1)
+                        writer.print(", ");
+                }
+                writer.println();
+            }
+
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
