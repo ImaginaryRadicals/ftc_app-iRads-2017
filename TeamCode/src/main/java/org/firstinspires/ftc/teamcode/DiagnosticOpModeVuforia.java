283014b7a6441048793298404aab9b8a44ee3d57
Ashley Trowell <ajtrowell@gmail.com>
2017-12-14 14:25:50 -0600

    Added SimpleVuforia pictograph reader and Diagnostic Vuforia opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiagnosticOpModeVuforia.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.teamcode.Utilities.Color;
+import org.firstinspires.ftc.teamcode.Vision.SimpleVuforia;
+
+
+/**
+ * Created by Ashley on 12/14/2017.
+ *
+ * Tests
+ */
+
+@TeleOp(name="Diagnostic VuforiaGlyph", group="diagnostic")
+public class DiagnosticOpModeVuforia extends DiagnosticOpMode {
+
+    private SimpleVuforia vuforia;
+
+    @Override
+    public void init() {
+        super.init();
+        vuforia = new SimpleVuforia(getVuforiaLicenseKey());
+        telemetry.addData("Diagnostic Vuforia Mode ", " Initialized");
+    }
+
+    @Override
+    public void loop() {
+        RelicRecoveryVuMark vuMark = vuforia.detectMark();
+        telemetry.addData("Vuforia Glyph Position", vuMark.name());
+
+        super.loop();
+    }
+
+}
