37e46efd14348e5b19d138e8da15412b66b2a213
Ashley Trowell <ajtrowell@gmail.com>
2017-12-20 01:49:41 -0600

    CSV: Added docs, Exception throws and write() calls.
    Call write() in  addRecord(), open(),  and close().
    Most functions will call write() to attempt to write 
    unwritten records to the file immediately.
    
    close() returns the number of unwritten records 
    or -1 in the case of an error.
    
    Docs added.
    Exceptions thrown for improperly sized records or 
    using csvWriteAndClose() after using 
    addFieldToRecord() and completeRecord()
    
    Improperly using NullPointerException, because it 
    is handled by ftc app.
    
    Reconstructed previous operation in 
    csvWriteAndClose().

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/CSV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/CSV.java
@@ -18,37 +18,57 @@ public class CSV {
     private PrintWriter writer;
     private Vector<Double> currentRecord = new Vector<>();
     private int numRecordsWritten = 0;
-    boolean isTitlesPopulated = false;
-    boolean isTitlesWritten = false;
-    boolean isOpen = false;
+    private boolean isTitlesPopulated = false;
+    private boolean isTitlesWritten = false;
+    private boolean isOpen = false;
 
     public CSV(RobotHardware robotHardware)
     {
         this.robotHardware = robotHardware;
     }
 
+    /**
+     * Add a new data record to the data table.
+     * Automatically attempts to write() when a records is added.
+     * @param new_data_record A vector of Doubles, representing the fields a a record.
+     */
     public void addRecord(Vector<Double> new_data_record)
     {
         dataTable.add(new_data_record);
+        write();
     }
+
     public void addRecord(double[] new_data_record)
     {
         Vector<Double> vec = new Vector<>();
         for (double value : new_data_record)
             vec.add(value);
         dataTable.add(vec);
+        write();
     }
 
+    /**
+     * Opens a FileOutputStream and a PrintWriter, and sets isOpen = true.
+     * CSV will automatically attempt write all data cached in data_table.
+     * @param fileName
+     */
     public void open(String fileName) {
         try{
             FileOutputStream outputStream = robotHardware.hardwareMap.appContext.openFileOutput(fileName, Context.MODE_PRIVATE);
             writer = new PrintWriter(outputStream);
             isOpen = true;
+            write();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Added field data to next record.
+     * Used in conjunction with completeRecord()
+     * @param title
+     * @param dataField
+     */
     public void addFieldToRecord(String title, Double dataField) {
         currentRecord.add(dataField);
         if(!isTitlesPopulated) {
@@ -56,13 +76,27 @@ public class CSV {
         }
     }
 
+    /**
+     * Adds current record to data_table, and attempts to write it.
+     */
     public void completeRecord() {
-        isTitlesPopulated = true;
-        addRecord((Vector<Double>)currentRecord.clone());
-        currentRecord.clear();
-        write();
+        if(currentRecord.size() > 0) {
+            if (dataTable.size() == 0 || currentRecord.size() == dataTable.lastElement().size()) {
+                isTitlesPopulated = true;
+                addRecord((Vector<Double>) currentRecord.clone());
+                currentRecord.clear();
+            } else { // Size error
+                throw new NullPointerException("Current Record is not the same size as previous Record.");
+            }
+        }
     }
 
+    /**
+     * If CSV is not open, does nothing.
+     * If the first record has not yet been closed (no titles populated), does nothing.
+     * If titles are populated but not written, it writes the titles to file.
+     * If titles are written, writes all unwritten dataRecords from dataTable to file.
+     */
     private void write() {
         try{
             // Exit write() if file is not open.
@@ -100,52 +134,48 @@ public class CSV {
         }
     }
 
-    public void close() {
+    /**
+     * Closes the writer file stream after attempting to write cached data.
+     * @return unwrittenRecords
+     */
+
+    public int close() {
         try{
             write();
             writer.close();
             isOpen = false;
+            int unwrittenRecords = dataTable.size() - numRecordsWritten;
+            return unwrittenRecords;
         } catch (Exception e) {
             e.printStackTrace();
+            return -1;
         }
     }
 
-    public void csvWriteAndClose(String fileName)
-    {
-        csvWriteAndClose(fileName, new Vector<String>());
-    }
 
+
+    /**
+     * Depreciated. Can open, write, and close in one step.
+     * Will throw error if used after calling addFieldToRecord().
+     * @param fileName
+     * @param titles
+     */
+    @Deprecated
     public void csvWriteAndClose(String fileName, Vector<String> titles)
     {
         try{
-            FileOutputStream outputStream = robotHardware.hardwareMap.appContext.openFileOutput(fileName, Context.MODE_PRIVATE);
-
-            PrintWriter writer = new PrintWriter(outputStream);
-
-            for (int i = 0; i < titles.size(); ++i)
-            {
-                writer.print(titles.get(i));
-                if (i < titles.size() - 1)
-                    writer.print(", ");
+            if (!isTitlesPopulated) {
+                this.titles = (Vector<String>)titles.clone();
+                isTitlesPopulated = true;
+            } else {
+                throw new NullPointerException("Titles already populated, can't over write. " +
+                        "Don't use csvWriteAndClose() and addFieldToRecord() together.");
             }
-            if (titles.size() > 0)
-                writer.println();
-
-            int n_steps = dataTable.size();
-            for (int i = 0; i < n_steps; ++i)
-            {
-                int n_states = dataTable.firstElement().size();
-                for (int j = 0; j < n_states; ++j)
-                {
-                    writer.print(dataTable.get(i).get(j));
-                    if (j < n_states - 1)
-                        writer.print(", ");
-                }
-                writer.println();
-            }
-
-            writer.close();
+            open(fileName);
+            write();
+            close();
         } catch (Exception e) {
+            robotHardware.telemetry.addData("Error:",e.getMessage());
             e.printStackTrace();
         }
     }
